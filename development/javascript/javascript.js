e8c3bc1fcef78fb810d06051ce97412be137bcfb
Huw Williams <huwmorgan91@gmail.com>
2022-10-18 12:45:36 +0100

    Made a js file and added in the challenges

---
+++ development/javascript/javascript.js
@@ -0,0 +1,11 @@
+// If a name, is a coaches name, log "It's a coach"
+
+//If a greeting, is bonjour, log "comment ca va"
+
+//If a score, raches ten, log "you won the game"
+
+//If age, is greater than 65, "Time to retire"
+
+//If has Covid, log "time to isolate", else "Keep social distancing"
+
+//If age greater than 18, and is British, "You can vote in the next election"
