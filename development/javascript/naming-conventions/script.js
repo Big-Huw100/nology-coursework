5c86ea83798da13d2fc37ff99874c7d9681dfeec
Huw Williams <huwmorgan91@gmail.com>
2022-10-20 11:45:17 +0100

    Finished debugging code along and stack overflow

--- development/javascript/naming-conventions/script.js
+++ development/javascript/naming-conventions/script.js
@@ -1,7 +1,7 @@
 // 1. Let's do something with some headlines
 
 // Use cammel casing for variables
-const currentHeadline = "News in the world today"
+let currentHeadline = "News in the world today"
 const currentArticle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget mi eget purus laoreet tristique. Cras quis ex rhoncus, convallis est a, aliquet elit. Pellentesque id enim odio. Fusce vel suscipit lectus. Phasellus maximus non ex venenatis hendrerit. Suspendisse tempor ligula eget velit ornare, quis pellentesque augue dapibus. Nulla facilisis erat a nulla suscipit, quis commodo nibh posuere. Suspendisse finibus, risus quis efficitur faucibus, purus orci congue justo, eget ullamcorper diam lectus non mi. Nullam vel quam leo. Vestibulum a ullamcorper massa, vel euismod nunc. Aenean dignissim rutrum tortor. Nam aliquam enim sem, tincidunt suscipit enim aliquet eget. Sed aliquam, nunc ac consequat imperdiet, neque tellus fringilla mauris, in ullamcorper lorem odio vitae nibh. Lorem ipsum dolor sit amet."
 
 // Use has for boolean and amountOf for number
@@ -9,14 +9,14 @@ const hasPassedTest = true;
 const amountOfWords = 957;
 
 // 2. Before we start we need to make sure the headline isn't too long
-if (headline.length > 20) {
-    headline = headline + "...";
+if (currentHeadline.length > 15) {
+    currentHeadline = currentHeadline.substring(0, 15) + "...";
 }
 
 // 3. Show it to the user
 console.log("Nology Post 2021")
-console.log(`Headline: $(headline)`)
-console.log(article);
+console.log("Headline: " + currentHeadline)
+console.log(currentArticle);
 
 
 // 4. Example of a meaningful function name.. getData() is vague :( getPost is more meaningful :)
