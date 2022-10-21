45359c9845a09ffadd5b0fa29479b1f1243dd209
Huw Williams <huwmorgan91@gmail.com>
2022-10-21 10:57:50 +0100

    created a for loop

--- development/javascript/arrays/script.js
+++ development/javascript/arrays/script.js
@@ -14,3 +14,8 @@ console.log(pets[2]);
 const numbers = [1, 46, 69, 100];
 console.log(numbers[2]);
 
+// 4. create a sample for loop
+
+for (let i = 0; i < 5; i++) {
+    console.log(`The index on this loop was ${i}`);
+}
