e44f4bca5a5f9563d387a28d34c72d48a97a4861
Huw Williams <huwmorgan91@gmail.com>
2022-10-21 10:47:31 +0100

    Finished arrays code along

---
+++ development/javascript/arrays/script.js
@@ -0,0 +1,16 @@
+// 1. create a coaches array
+
+const coaches = ["Andy", "Sam", "Bex"];
+console.log(coaches[1]);
+
+// 2. create an array of pets
+
+const pets = ["Fido", "Rusty", "Jack"];
+console.log(pets[2]);
+
+
+// 3. create and array of numbers
+
+const numbers = [1, 46, 69, 100];
+console.log(numbers[2]);
+
