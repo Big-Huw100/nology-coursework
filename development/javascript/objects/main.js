76318d6101fe2b22413fb58e5540277512af777a
Huw Williams <huwmorgan91@gmail.com>
2022-11-15 12:42:55 +0000

    Finished Objects Code Along Object and the DOM

--- development/javascript/objects/main.js
+++ development/javascript/objects/main.js
@@ -27,11 +27,14 @@
 
 // console.log(exampleObject);
 
+const studentContainer = document.querySelector(".students-container");
+
 const person = {
     firstName: "Huw",
     lastName: "Williams",
     quote: "It's what we do with the gift of life that determins who we are",
     skills: ["HTML", "CSS", "Javascript"],
+    imgURL: "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/288020499_10161560298226756_2295376958204827870_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=8jlh0fKAnAEAX_0JRB3&_nc_ht=scontent-man2-1.xx&oh=00_AfA6H920aViQDcyTGLiXnbtYHX9dCHFMh-sB0IRkXUxUfA&oe=6377DFA0",
     getFullName() {
         return `${this.firstName} ${this.lastName}`
     },
@@ -39,23 +42,21 @@ const person = {
         const skillList = this.skills.map(skill => `<li>${skill}</li>`)
         console.log(skillList);
         return skillList.join("\n");
+    },
+    getPersonHTML() {
+        const personHTML = `
+        <article class="student-card">
+            <img src=${this.imgURL} />
+            <h2>${this.getFullName()}</h2>
+            <blockquote>${this.quote}</blockquote>
+            <h3>Skills</h3>
+            <ul>${this.getSkillList()}</ul>
+        </article>
+        `
+        return personHTML;
     }
 };
 
-console.log(person.getSkillList());
-
-// Challeneg: write a method in the person object that uses the skills array
-//Iterate over the skills array and generate a list item for each skill
-// e.g. <li>html</li>
-// need to return a string -> array into a string
+studentContainer.innerHTML = person.getPersonHTML();
 
 person.age = 31;
-// console.log(person);
-
-// // Challenge: Get first name and last name onto the console
-
-// console.log(person.firstName, person.lastName);
-
-// // Challenge: Get the method onto the console
-
-// console.log(person.getFullName());
