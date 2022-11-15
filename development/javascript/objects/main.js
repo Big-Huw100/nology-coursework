15862932fb163063a836a3a57d47eed2cc010cbd
Huw Williams <huwmorgan91@gmail.com>
2022-11-15 12:18:19 +0000

    Finished Objects Mini Challenege and Review

--- development/javascript/objects/main.js
+++ development/javascript/objects/main.js
@@ -30,18 +30,32 @@
 const person = {
     firstName: "Huw",
     lastName: "Williams",
+    quote: "It's what we do with the gift of life that determins who we are",
+    skills: ["HTML", "CSS", "Javascript"],
     getFullName() {
         return `${this.firstName} ${this.lastName}`
+    },
+    getSkillList() {
+        const skillList = this.skills.map(skill => `<li>${skill}</li>`)
+        console.log(skillList);
+        return skillList.join("\n");
     }
 };
 
+console.log(person.getSkillList());
+
+// Challeneg: write a method in the person object that uses the skills array
+//Iterate over the skills array and generate a list item for each skill
+// e.g. <li>html</li>
+// need to return a string -> array into a string
+
 person.age = 31;
-console.log(person);
+// console.log(person);
 
-// Challenge: Get first name and last name onto the console
+// // Challenge: Get first name and last name onto the console
 
-console.log(person.firstName, person.lastName);
+// console.log(person.firstName, person.lastName);
 
-// Challenge: Get the method onto the console
+// // Challenge: Get the method onto the console
 
-console.log(person.getFullName());
+// console.log(person.getFullName());
