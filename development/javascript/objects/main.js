1db2d4035e4327e33210992cc00279eca454f687
Huw Williams <huwmorgan91@gmail.com>
2022-11-15 15:15:13 +0000

    Finished the Object and Classes videos

--- development/javascript/objects/main.js
+++ development/javascript/objects/main.js
@@ -68,23 +68,38 @@
 // Class Syntax
 
 // PascalCase -> naming convention for classes
-class ExampleClass {
-    constructor(firstName, lastName, age) {
-        (this.firstName = firstName),
-        (this.lastName = lastName),
-        (this.age = age)
-    }
+// class ExampleClass {
+//     constructor(firstName, lastName, age) {
+//         (this.firstName = firstName),
+//         (this.lastName = lastName),
+//         (this.age = age)
+//     }
 
-    greeting() {
-        return `Hi, my name is ${this.firstName} ${this.lastName} and I am ${this.age} years old`;
-    };
-};
+//     greeting() {
+//         return `Hi, my name is ${this.firstName} ${this.lastName} and I am ${this.age} years old`;
+//     };
+// };
 
-const exampleOne = new ExampleClass("Huw", "Williams", 31);
-const exampleTwo = new ExampleClass("John", "Doe", 27)
+// // Class Inheritance Syntax
 
-console.log(exampleOne.greeting());
-console.log(exampleTwo.greeting());
+// class ExtendedClass extends ExampleClass {
+//     constructor(firstName, lastName, age, subject) {
+//         super(firstName, lastName, age),
+//         this.subject = subject;
+//     }
+
+//     greeting() {
+//         return `Hi I am Mr ${this.lastName} I will teach you ${this.subject}`;
+//     }
+// }
+
+// console.log(new ExtendedClass("Charlie", "Richardson", 30, "javascript").greeting());
+
+// const exampleOne = new ExampleClass("Huw", "Williams", 31);
+// const exampleTwo = new ExampleClass("John", "Doe", 27)
+
+// console.log(exampleOne.greeting());
+// console.log(exampleTwo.greeting());
 
 class Profile {
     constructor(firstName, lastName, quote, skills, imgURL) {
@@ -112,14 +127,35 @@ class Profile {
             <h3>Skills</h3>
             <ul>${this.getSkillList()}</ul>
         </article>
-        `
+        `;
+        return profileHTML;
+    }
+};
+
+class PremiumProfile extends Profile {
+    constructor(firstName, lastName, quote, skills, imgURL, contactLink) {
+        super(firstName, lastName, quote, skills, imgURL)
+        this.contactLink = contactLink
+    }
+
+    getProfileHTML() {
+        const profileHTML = `
+        <article class="student-card">
+            <img src=${this.imgURL} />
+            <h2>${this.getFullName()}</h2>
+            <blockquote>${this.quote}</blockquote>
+            <h3>Skills</h3>
+            <ul>${this.getSkillList()}</ul>
+            <a href = ${this.contactLink}>Lets get in touch!</a>
+        </article>
+        `;
         return profileHTML;
     }
 };
 
 const studentContainer = document.querySelector(".students-container");
 
-const huw = new Profile("Huw", "Williams", "It's what we do with the gift of life that determins who we are", ["HTML", "CSS", "Javascript"], "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/288020499_10161560298226756_2295376958204827870_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=8jlh0fKAnAEAX_0JRB3&_nc_ht=scontent-man2-1.xx&oh=00_AfA6H920aViQDcyTGLiXnbtYHX9dCHFMh-sB0IRkXUxUfA&oe=6377DFA0");
+const huw = new PremiumProfile("Huw", "Williams", "It's what we do with the gift of life that determins who we are", ["HTML", "CSS", "Javascript"], "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/288020499_10161560298226756_2295376958204827870_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=8jlh0fKAnAEAX_0JRB3&_nc_ht=scontent-man2-1.xx&oh=00_AfA6H920aViQDcyTGLiXnbtYHX9dCHFMh-sB0IRkXUxUfA&oe=6377DFA0", "https://www.google.com/");
 
 const john = new Profile("John", "Doe", "Them's the breaks!", ["Dancing", "Swimging", "Painting"], "https://scontent-man2-1.xx.fbcdn.net/v/t1.6435-9/66130321_10158147439021756_3648801646844051456_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=174925&_nc_ohc=lAXE4dFx_XkAX-HQpjT&_nc_ht=scontent-man2-1.xx&oh=00_AfD8RHdl4lEydkSqYmCCdt-XQH16T0lVYWDVUzkez2slrg&oe=639B0D34");
 
